1e934fa9dd4857f8fb4191601a48e23c75352ca9
Nick Rogers <ncrogers@gmail.com>
2012-02-23 15:37:19 -0500

    Export memory and performance improvements borrowed from the mojotech fork.

--- lib/active_scaffold/actions/export.rb
+++ lib/active_scaffold/actions/export.rb
@@ -5,7 +5,7 @@ module ActiveScaffold::Actions
       base.before_filter :init_session_var
 
       as_export_plugin_path = File.join(ActiveScaffold::Config::Export.plugin_directory, 'frontends', 'default' , 'views')
-      
+
       base.add_active_scaffold_path as_export_plugin_path
     end
 
@@ -43,38 +43,60 @@ module ActiveScaffold::Actions
         params.merge!(options)
       end
 
-      find_items_for_export
+      # this is required if you want this to work with IE
+      if request.env['HTTP_USER_AGENT'] =~ /msie/i
+        response.headers['Pragma'] = "public"
+        response.headers['Cache-Control'] = "no-cache, must-revalidate, post-check=0, pre-check=0"
+        response.headers['Expires'] = "0"
+      end
 
+      response.headers['Content-type'] = 'text/csv'
       response.headers['Content-Disposition'] = "attachment; filename=#{export_file_name}"
-      render :partial => 'export', :layout => false, :content_type => Mime::CSV, :status => response_status
+
+      @export_columns = export_config.columns.reject { |col| params[:export_columns][col.name.to_sym].nil? }
+      includes_for_export_columns = @export_columns.collect{ |col| col.includes }.flatten.uniq.compact
+      self.active_scaffold_includes.concat includes_for_export_columns
+      @export_config = export_config
+
+      # start streaming output
+      self.response_body = proc { |response, output|
+        find_items_for_export do |records|
+          @records = records
+          str = render_to_string :partial => 'export', :layout => false
+          output.write(str)
+          params[:skip_header] = 'true' # skip header on the next run
+        end
+      }
     end
 
     protected
-
     # The actual algorithm to do the export
-    def find_items_for_export
-      export_config = active_scaffold_config.export
-      export_columns = export_config.columns.reject { |col| params[:export_columns][col.name.to_sym].nil? }
-
-      includes_for_export_columns = export_columns.collect{ |col| col.includes }.flatten.uniq.compact
-      self.active_scaffold_includes.concat includes_for_export_columns
-
-      find_options = { :sorting => active_scaffold_config.list.user.sorting }
+    def find_items_for_export(&block)
+      find_options = { :sorting =>
+        active_scaffold_config.list.user.sorting.nil? ?
+          active_scaffold_config.list.sorting : active_scaffold_config.list.user.sorting,
+        :pagination => true
+      }
       params[:search] = session[:search]
       do_search rescue nil
       params[:segment_id] = session[:segment_id]
       do_segment_search rescue nil
-      unless params[:full_download] == 'true'
+
+      if params[:full_download] == 'true'
         find_options.merge!({
-          :pagination => active_scaffold_config.list.pagination,
+          :per_page => 10000,
+          :page => 1
+        })
+        find_page(find_options).pager.each do |page|
+          yield page.items
+        end
+      else
+        find_options.merge!({
           :per_page => active_scaffold_config.list.user.per_page,
           :page => active_scaffold_config.list.user.page
         })
+        yield find_page(find_options).items
       end
-
-      @export_config = export_config
-      @export_columns = export_columns
-      @records = find_page(find_options).items
     end
 
     # The default name of the downloaded file.
